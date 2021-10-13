2811728a377fbc2643ae452b25feb7215f824cac
yin1999 <15844309+yin1999@users.noreply.github.com>
2021-10-13 13:22:37 +0800

    fotmat

--- 404/index.js
+++ 404/index.js
@@ -2,24 +2,24 @@
 
 const el = document.querySelector('.box__ghost-eyes')
 
-document.onmousemove = function (event) {
-    //verticalAxis
-    let mouseY = event.pageY;
-    let yAxis = 150 - mouseY / window.innerHeight * 300;
-    if (yAxis < 0) {
-        yAxis = 0;
-    }
-    //horizontalAxis
-    let mouseX = event.pageX / window.innerWidth;
-    let xAxis = mouseX * 100 - 100;
+document.onmousemove = event => {
+	//verticalAxis
+	const mouseY = event.pageY
+	let yAxis = 150 - mouseY / window.innerHeight * 300
+	if (yAxis < 0) {
+		yAxis = 0
+	}
+	//horizontalAxis
+	const mouseX = event.pageX / window.innerWidth
+	const xAxis = mouseX * 100 - 100
 
-    el.style.transform = 'translate(' + xAxis + '%,-' + yAxis + '%)';
+	el.style.transform = 'translate(' + xAxis + '%,-' + yAxis + '%)'
 }
 
 function goBack() {
-    if (history.length >= 2) {
-        history.back();
-    } else {
-        window.close()
-    }
+	if (history.length >= 2) {
+		history.back()
+	} else {
+		window.close()
+	}
 }
